7a81fb4f13da177c98a4ec1e05828196ccc5ca73
smerrell <merrell.sam@gmail.com>
2011-07-21 15:26:54 -0600

    Added ugly hack to detect my non 256 color terminal

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -1,27 +1,27 @@
 # Taken from: http://winterdom.com/2008/08/mypowershellprompt With a couple modifications.
 
-$homeDir = "c:\Projects"
+Set-Alias gtab "c:\Projects\gvim.bat"
 
-function shorten-path([string] $path) { 
-   $loc = $path.Replace($HOME, '~') 
-   # remove prefix for UNC paths 
-   $loc = $loc -replace '^[^:]+::', '' 
-   # make path shorter like tabs in Vim, 
-   # handle paths starting with \\ and . correctly 
-   return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2') 
+function shorten-path([string] $path) {
+   $loc = $path.Replace($HOME, '~')
+   # remove prefix for UNC paths
+   $loc = $loc -replace '^[^:]+::', ''
+   # make path shorter like tabs in Vim,
+   # handle paths starting with \\ and . correctly
+   return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2')
 }
 
 function Test-Admin {
  $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
  $principal = new-object Security.Principal.WindowsPrincipal $identity
- $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)   
+ $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
-function prompt { 
-   # our theme 
-   $cdelim = [ConsoleColor]::DarkCyan 
-   $chost = [ConsoleColor]::Green 
-   $cloc = [ConsoleColor]::Cyan 
+function prompt {
+   # our theme
+   $cdelim = [ConsoleColor]::DarkCyan
+   $chost = [ConsoleColor]::Green
+   $cloc = [ConsoleColor]::Cyan
    $time = get-date -u "%H:%M"
    # So we know if the shell is running as admin
    $adminTag = ""
@@ -32,11 +32,9 @@ function prompt {
       $adminTag = ">>>"
    }
 
-   write-host "$adminTag [$time]" ([net.dns]::GetHostName()) -n -f $chost 
-   write-host ' {' -n -f $cdelim 
-   write-host ((pwd).Path) -n -f $cloc 
-   write-host '}' -f $cdelim 
-   return '$ ' 
+   write-host "$adminTag [$time]" ([net.dns]::GetHostName()) -n -f $chost
+   write-host ' {' -n -f $cdelim
+   write-host ((pwd).Path) -n -f $cloc
+   write-host '}' -f $cdelim
+   return '$ '
 }
-
-cd $homeDir
