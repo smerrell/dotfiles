21a2f11705efbcfb55869cb6f43bd7828fb81afd
Sam Merrell <merrell.sam@gmail.com>
2012-11-03 23:38:35 -0600

    Initial OSX defaults config
    * Mostly borrowed from Zach Holman's config
    * Will update this more as I figure out what my preferred defaults for
      OSX are

---
+++ osx/set-defaults.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Use AirDrop over every interface.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Always open everything in Finder's list view.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Safari Defaults mostly for dev.
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
