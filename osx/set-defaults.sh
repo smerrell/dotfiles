71e706f60bc75b1b58b45182a600e9d48bdfd543
Sam Merrell <merrell.sam@gmail.com>
2013-04-19 17:28:32 -0600

    Update keyboard defaults for OSX

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -13,3 +13,9 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Keyboard changes
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 0
