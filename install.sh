0b64546b309c4e14b5aa04d084cd1c10747e2a0a
Sam Merrell <merrell.sam@gmail.com>
2014-04-27 02:08:28 -0600

    Add update_bundles script
    Make life easier when updating vim bundles

--- install.sh
+++ install.sh
@@ -47,9 +47,5 @@ done
 
 link_bin
 
-git submodule sync
-git submodule init
-git submodule update
-git submodule foreach git pull origin master
-git submodule foreach git submodule init
-git submodule foreach git submodule update
+./update_bundles.sh
+
