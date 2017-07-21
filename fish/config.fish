63ec00f44415d95b66b9635c85075d2c7837f4ea
Sam Merrell <merrell.sam@gmail.com>
2017-07-21 16:36:01 -0600

    Add bin files to path in fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,16 @@
 set -gx LSCOLORS gxfxcxdxbxegedabagacad
+
+set fish_greeting ""
+
+echo "Adding SSH keys"
+eval "ssh-agent -s" > /dev/null
+ssh-add -K /Users/smerrell/.ssh/wtw_rsa
+ssh-add -K /Users/smerrell/.ssh/github_id_rsa
+
+set -l additional_path_items "$HOME/bin" "$HOME/.cargo/bin"
+for p in $additional_path_items
+    if test -d $p; and not contains $i $PATH
+	set PATH $p $PATH
+    end
+end
+
