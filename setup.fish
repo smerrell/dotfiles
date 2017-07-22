eea9793438fdefbe89f54d01d4f4796be2faee17
Sam Merrell <merrell.sam@gmail.com>
2017-07-22 11:20:02 -0600

    Change how links into ~/.config work

--- setup.fish
+++ setup.fish
@@ -6,6 +6,7 @@ else
     echo "~/.config exists, skipping"
 end
 
+# symlinks into $HOME
 for linkable in _*
     set linkdir (string replace -r "^_" "." $linkable)
     if not test -L "$HOME/$linkdir"
@@ -14,6 +15,26 @@ for linkable in _*
         echo "$HOME/$linkdir already symlinked, skipping"
     end
 end
+
+# symlinks into $HOME/.config
+pushd config
+for dir in *
+    # Not sure how to avoid this since it looks like fish creates the
+    # $HOME/config/fish folder once this script is run. Once that is
+    # done, symlinking ends up going to $HOME/config/fish/fish...
+    if string match "fish" $dir
+        pushd $dir
+        for file in *
+            ln -sv "$PWD/$file" "$HOME/.config/fish/$file"
+        end
+        popd
+    else if not test -L "$HOME/.config/$dir"
+        ln -sv "$PWD/$dir" "$HOME/.config/"
+    else
+        echo "$HOME/.config/$dir already symlinked, skipping"
+    end
+end
+popd
 
 git submodule init
 git submodule update
