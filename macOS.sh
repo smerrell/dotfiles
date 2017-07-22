77b09b0ba1338990482f2a9a75894b082c986c67
Sam Merrell <merrell.sam@gmail.com>
2017-07-22 15:22:50 -0600

    Only try to install brew when not installed

--- macOS.sh
+++ macOS.sh
@@ -3,12 +3,22 @@
 echo "Bootstrapping macOS machine"
 xcode-select --install 2&> /dev/null
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+which -s brew
+if [[ $? != 0 ]]; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "Brew installed, running an update"
+    brew update
+fi
 
 brew install fish
 
 # set fish as a usable shell
-sudo echo "/usr/local/bin/fish" >> /etc/shells
+if ! grep -q "fish" /etc/shells; then
+    sudo echo "/usr/local/bin/fish" >> /etc/shells
+else
+    echo "fish already in /etc/shells, skipping."
+fi
 
 # set fish as default shell
 chsh -s /usr/local/bin/fish
