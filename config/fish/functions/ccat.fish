5d4633b10501e6cc2b12633b0b113221b3956cc9
Sam Merrell <merrell.sam@gmail.com>
2017-08-28 15:57:13 -0600

    Colorized cat without line numbers
    Felt like I don't really need the line nuymbers most of the time.
    lcat will do a colorized cat with line numbers if I want line numbers.

--- config/fish/functions/ccat.fish
+++ config/fish/functions/ccat.fish
@@ -2,6 +2,7 @@
 # sudo pip install Pygments
 
 function ccat
-    pygmentize -f terminal -O linenos=True,bg=dark -g $argv
+    #pygmentize -f terminal -O linenos=True,bg=dark -g $argv
+    pygmentize -f terminal -O bg=dark -g $argv
 end
 
