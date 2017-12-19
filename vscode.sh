db5122dc91e7a6df9e99f692edb073f17eab2eea
Sam Merrell <merrell.sam@gmail.com>
2017-12-19 16:27:36 -0700

    Update VS Code config script
    Can update Code or the Insiders version.
    Add some Azure extensions.

--- vscode.sh
+++ vscode.sh
@@ -1,28 +1,38 @@
 #!/usr/bin/env bash
 
-plugins=(
+vscodeInstance="code"
+if [ "$1" == "--insiders" ]; then
+    vscodeInstance="code-insiders"
+fi
+
+extensions=(
     "EditorConfig.editorconfig"
     "JPTarquino.postgresql"
     "Stephanvs.dot"
     "Tyriar.sort-lines"
+    "VisualStudioOnlineApplicationInsights.application-insights"
     "bungcip.better-toml"
     "cake-build.cake-vscode"
     "dracula-theme.theme-dracula"
     "esbenp.prettier-vscode"
     "jebbs.plantuml"
+    "ms-azuretools.vscode-azurefunctions"
     "ms-mssql.mssql"
+    "ms-vscode.azure-account"
     "ms-vscode.csharp"
     "ms-vscode.mono-debug"
     "ms-vscode.powershell"
+    "msazurermtools.azurerm-vscode-tools"
     "qhoekman.language-plantuml"
     "rust-lang.rust"
     "stkb.rewrap"
+    "streetsidesoftware.code-spell-checker"
     "vadimcn.vscode-lldb"
     "vscodevim.vim"
     "webfreak.debug"
 )
 
-for plugin in ${plugins[@]}; do
-    code --install-extension $plugin
+for extension in ${extensions[@]}; do
+    $vscodeInstance --install-extension $extension
 done
 
