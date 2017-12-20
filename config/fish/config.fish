63495b7a520205752f2b3d6ee97280f5113a9bde
Sam Merrell <merrell.sam@gmail.com>
2017-12-19 22:56:10 -0700

    Load ssh keys better

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,14 +2,50 @@ set -gx LSCOLORS gxfxcxdxbxegedabagacad
 
 set fish_greeting ""
 
-echo "Adding SSH keys"
-eval "ssh-agent -s" > /dev/null
-ssh add -A
-
 set -l additional_path_items "$HOME/bin" "$HOME/.cargo/bin"
 for p in $additional_path_items
     if test -d $p; and not contains $i $PATH
-	set PATH $p $PATH
+    set PATH $p $PATH
+    end
+end
+
+# Setting up the SSH Agent
+# https://gist.github.com/gerbsen/5fd8aa0fde87ac7a2cae
+setenv SSH_ENV $HOME/.ssh/environment
+
+function start_agent
+    echo "Initializing new SSH agent ..."
+    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    echo "succeeded"
+    chmod 600 $SSH_ENV
+    . $SSH_ENV > /dev/null
+    ssh-add
+end
+
+function test_identities
+    ssh-add -l | grep "The agent has no identities" > /dev/null
+    if [ $status -eq 0 ]
+        ssh-add
+        if [ $status -eq 2 ]
+            start_agent
+        end
+    end
+end
+
+if [ -n "$SSH_AGENT_PID" ]
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    end
+else
+    if [ -f $SSH_ENV ]
+        . $SSH_ENV > /dev/null
+    end
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    else
+        start_agent
     end
 end
 
